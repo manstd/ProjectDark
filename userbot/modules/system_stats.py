1eceebc2c9fa6570eadce1eb8af32e68243d01a8
DarkoBunny <darkobunny57@gmail.com>
2022-03-02 17:02:08 +0700

    Update system_stats.py

--- userbot/modules/system_stats.py
+++ userbot/modules/system_stats.py
@@ -1,123 +1,31 @@
 # Copyright (C) 2019 The Raphielscape Company LLC.
 #
-# Licensed under the Raphielscape Public License, Version 1.d (the "License");
+# Licensed under the Raphielscape Public License, Version 1.c (the "License");
 # you may not use this file except in compliance with the License.
 #
+""" Userbot module for getting information about the server. """
 
-import asyncio
-import platform
-import sys
-import time
 from asyncio import create_subprocess_exec as asyncrunapp
 from asyncio.subprocess import PIPE as asyncPIPE
-from datetime import datetime
-from os import remove
 from platform import python_version, uname
 from shutil import which
 
-import psutil
-from telethon import __version__, version
+from git import Repo
+from telethon import version
+from telethon.errors.rpcerrorlist import MediaEmptyError
 
-from userbot import (
-    ALIVE_LOGO,
-    ALIVE_NAME,
-    CMD_HELP,
-    UPSTREAM_REPO_BRANCH,
-    StartTime,
-    bot,
-)
+from userbot import ALIVE_LOGO, ALIVE_NAME, CMD_HELP
 from userbot.events import register
 
 # ================= CONSTANT =================
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
+repo = Repo()
 # ============================================
-
-modules = CMD_HELP
-
-
-async def get_readable_time(seconds: int) -> str:
-    count = 0
-    up_time = ""
-    time_list = []
-    time_suffix_list = ["s", "m", "h", "days"]
-
-    while count < 4:
-        count += 1
-        remainder, result = divmod(seconds, 60) if count < 3 else divmod(seconds, 24)
-        if seconds == 0 and remainder == 0:
-            break
-        time_list.append(int(result))
-        seconds = int(remainder)
-
-    for x in range(len(time_list)):
-        time_list[x] = str(time_list[x]) + time_suffix_list[x]
-    if len(time_list) == 4:
-        up_time += time_list.pop() + ", "
-
-    time_list.reverse()
-    up_time += ":".join(time_list)
-
-    return up_time
-
-
-@register(outgoing=True, pattern=r"^\.spc")
-async def psu(event):
-    uname = platform.uname()
-    softw = "**Informasi Sistem**\n"
-    softw += f"`Sistem   : {uname.system}`\n"
-    softw += f"`Rilis    : {uname.release}`\n"
-    softw += f"`Versi    : {uname.version}`\n"
-    softw += f"`Mesin    : {uname.machine}`\n"
-    # Boot Time
-    boot_time_timestamp = psutil.boot_time()
-    bt = datetime.fromtimestamp(boot_time_timestamp)
-    softw += f"`Boot Time: {bt.day}/{bt.month}/{bt.year}  {bt.hour}:{bt.minute}:{bt.second}`\n"
-    # CPU Cores
-    cpuu = "**CPU Info**\n"
-    cpuu += "`Physical cores   : " + str(psutil.cpu_count(logical=False)) + "`\n"
-    cpuu += "`Total cores      : " + str(psutil.cpu_count(logical=True)) + "`\n"
-    # CPU frequencies
-    cpufreq = psutil.cpu_freq()
-    cpuu += f"`Maks Frekuensi     : {cpufreq.max:.2f}Mhz`\n"
-    cpuu += f"`Min Frekuensi      : {cpufreq.min:.2f}Mhz`\n"
-    cpuu += f"`Frekuensi saat ini : {cpufreq.current:.2f}Mhz`\n\n"
-    # CPU usage
-    cpuu += "**Penggunaan CPU Per Core**\n"
-    for i, percentage in enumerate(psutil.cpu_percent(percpu=True)):
-        cpuu += f"`Core {i}  : {percentage}%`\n"
-    cpuu += "**Total Penggunaan CPU**\n"
-    cpuu += f"`Semua Core: {psutil.cpu_percent()}%`\n"
-    # RAM Usage
-    svmem = psutil.virtual_memory()
-    memm = "**Penggunaan Memori**\n"
-    memm += f"`Total     : {get_size(svmem.total)}`\n"
-    memm += f"`Tersedia  : {get_size(svmem.available)}`\n"
-    memm += f"`Digunakan : {get_size(svmem.used)}`\n"
-    memm += f"`Persentasi: {svmem.percent}%`\n"
-    # Bandwidth Usage
-    bw = "**Penggunaan Bandwidth**\n"
-    bw += f"`Unggah  : {get_size(psutil.net_io_counters().bytes_sent)}`\n"
-    bw += f"`Unduh   : {get_size(psutil.net_io_counters().bytes_recv)}`\n"
-    help_string = f"{str(softw)}\n"
-    help_string += f"{str(cpuu)}\n"
-    help_string += f"{str(memm)}\n"
-    help_string += f"{str(bw)}\n"
-    help_string += "**Engine Info**\n"
-    help_string += f"`Python {sys.version}`\n"
-    help_string += f"`Telethon {__version__}`"
-    await event.edit(help_string)
-
-
-def get_size(bytes, suffix="B"):
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
 
 
 @register(outgoing=True, pattern=r"^\.sysd$")
 async def sysdetails(sysd):
+    """For .sysd command, get system info using neofetch."""
     if not sysd.text[0].isalpha() and sysd.text[0] not in ("/", "#", "@", "!"):
         try:
             fetch = await asyncrunapp(
@@ -132,150 +40,88 @@ async def sysdetails(sysd):
 
             await sysd.edit("`" + result + "`")
         except FileNotFoundError:
-            await sysd.edit("`Install neofetch dahulu !!`")
+            await sysd.edit("`Install neofetch first !!`")
 
 
 @register(outgoing=True, pattern=r"^\.botver$")
 async def bot_ver(event):
-    if event.text[0].isalpha() or event.text[0] in ("/", "#", "@", "!"):
-        return
-    if which("git") is not None:
-        ver = await asyncrunapp(
-            "git",
-            "describe",
-            "--all",
-            "--long",
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await ver.communicate()
-        verout = str(stdout.decode().strip()) + str(stderr.decode().strip())
+    """For .botver command, get the bot version."""
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        if which("git") is not None:
+            ver = await asyncrunapp(
+                "git",
+                "describe",
+                "--all",
+                "--long",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+            stdout, stderr = await ver.communicate()
+            verout = str(stdout.decode().strip()) + str(stderr.decode().strip())
 
-        rev = await asyncrunapp(
-            "git",
-            "rev-list",
-            "--all",
-            "--count",
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-        stdout, stderr = await rev.communicate()
-        revout = str(stdout.decode().strip()) + str(stderr.decode().strip())
+            rev = await asyncrunapp(
+                "git",
+                "rev-list",
+                "--all",
+                "--count",
+                stdout=asyncPIPE,
+                stderr=asyncPIPE,
+            )
+            stdout, stderr = await rev.communicate()
+            revout = str(stdout.decode().strip()) + str(stderr.decode().strip())
 
-        await event.edit(
-            "`ProjectDark Versi: " f"{verout}" "` \n" "`Revision: " f"{revout}" "`"
-        )
-    else:
-        await event.edit(
-            "Shame that you don't have git, you're running - 'v1.beta.4' anyway!"
-        )
-
-
-@register(outgoing=True, pattern=r"^\.pip(?: |$)(.*)")
-async def pipcheck(pip):
-    if pip.text[0].isalpha() or pip.text[0] in ("/", "#", "@", "!"):
-        return
-    pipmodule = pip.pattern_match.group(1)
-    if pipmodule:
-        await pip.edit("`Mencari . . .`")
-        pipc = await asyncrunapp(
-            "pip3",
-            "search",
-            pipmodule,
-            stdout=asyncPIPE,
-            stderr=asyncPIPE,
-        )
-
-        stdout, stderr = await pipc.communicate()
-        pipout = str(stdout.decode().strip()) + str(stderr.decode().strip())
-
-        if pipout:
-            if len(pipout) > 4096:
-                await pip.edit("`Output too large, sending as file`")
-                file = open("output.txt", "w+")
-                file.write(pipout)
-                file.close()
-                await pip.client.send_file(
-                    pip.chat_id,
-                    "output.txt",
-                    reply_to=pip.id,
-                )
-                remove("output.txt")
-                return
-            await pip.edit(
-                "**Kueri: **\n`"
-                f"pip3 search {pipmodule}"
-                "`\n**Hasil: **\n`"
-                f"{pipout}"
-                "`"
+            await event.edit(
+                "`Userbot Version: " f"{verout}" "` \n" "`Revision: " f"{revout}" "`"
             )
         else:
-            await pip.edit(
-                "**Kueri: **\n`"
-                f"pip3 search {pipmodule}"
-                "`\n**Hasil: **\n`Tidak Ada Hasil/False`"
+            await event.edit(
+                "Shame that you don't have git, you're running - 'v1.beta.4' anyway!"
             )
-    else:
-        await pip.edit("`Use .help pip to see an example`")
 
 
-@register(outgoing=True, pattern=r"^\.(?:alive|on)\s?(.)?")
+@register(outgoing=True, pattern=r"^\.(alive|on)$")
 async def amireallyalive(alive):
-    user = await bot.get_me()
-    await get_readable_time((time.time() - StartTime))
+    """For .alive command, check if the bot is running."""
     output = (
-        f"**ProjectDark** berjalan di {UPSTREAM_REPO_BRANCH}\n"
-        f"▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱\n"
-        f"👤 `User          :` {DEFAULTUSER}\n"
-        f"👁‍🗨 `Username      :` @{user.username}\n"
-        f"🐍 `Python        :` v{python_version()}\n"
-        f"⚙️ `Telethon      :` v{version.__version__}\n"
-        f"▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱▱"
+        f"**ProjectDark** is running on **{repo.active_branch.name}**\n\n"
+        f"**Python :** __v{python_version()}__\n"
+        f"**Telethon :** __v{version.__version__}__\n"
+        f"**User :** __{DEFAULTUSER}__"
     )
     if ALIVE_LOGO:
         try:
-            logo = ALIVE_LOGO
+            await alive.respond(output, file=ALIVE_LOGO)
             await alive.delete()
-            msg = await bot.send_file(alive.chat_id, logo, caption=output)
-            await asyncio.sleep(360)
-            await msg.delete()
-        except BaseException:
+        except MediaEmptyError:
             await alive.edit(
                 output + "\n\n *`The provided logo is invalid."
                 "\nMake sure the link is directed to the logo picture`"
             )
-            await asyncio.sleep(360)
-            await alive.delete()
     else:
         await alive.edit(output)
-        await asyncio.sleep(360)
-        await alive.delete()
 
 
-@register(outgoing=True, pattern=r"^\.aliveu")
+@register(outgoing=True, pattern=r"^\.aliveu (.+)")
 async def amireallyaliveuser(username):
-    message = username.text
-    output = ".aliveu [new user without brackets] nor can it be empty"
-    if message != ".aliveu" and message[7:8] == " ":
-        newuser = message[8:]
-        global DEFAULTUSER
-        DEFAULTUSER = newuser
-        output = "Sukses mengubah pengguna ke " + newuser + "!"
-    await username.edit("`" f"{output}" "`")
+    """For .aliveu command, change the username in the .alive command."""
+    newuser = username.pattern_match.group(1)
+    global DEFAULTUSER
+    DEFAULTUSER = newuser
+    await username.edit(f"Successfully changed user to `{newuser}`")
 
 
 @register(outgoing=True, pattern=r"^\.resetalive$")
 async def amireallyalivereset(ureset):
+    """For .resetalive command, reset the username in the .alive command."""
     global DEFAULTUSER
     DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else uname().node
-    await ureset.edit("`" "Sukses atur ulang pengguna untuk alive!" "`")
+    await ureset.edit("`" "Successfully reset user for alive!" "`")
 
 
 CMD_HELP.update(
     {
         "sysd": ">`.sysd`" "\nUsage: Shows system information using neofetch.",
         "botver": ">`.botver`" "\nUsage: Shows the userbot version.",
-        "pip": ">`.pip <module(s)>`" "\nUsage: Does a search of pip modules(s).",
         "alive": ">`.alive`"
         "\nUsage: Type .alive to see wether your bot is working or not."
         "\n\n>`.aliveu <text>`"
